5b593b753ea82f34bdd436b73820a5ee93e5a1a8
Pedro H de Souza <pedroh.soouza@gmail.com>
2020-08-28 23:15:35 -0300

    Train and plot loss and accuray
    English:
    This coding is ready to train an plot the  loss and accuracy of the model. Important: The model does not has the better param and configs.
    
    Português:
    Código para treinar e plotar a acuracia e o loss do modelo, mas ainda não está com o modelo bem acertado.

--- script/dogvswolf.py
+++ script/dogvswolf.py
@@ -63,4 +63,52 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE
 train_data = train_data.cache().shuffle(300).prefetch(buffer_size=AUTOTUNE)
 test_data = test_data.cache().prefetch(buffer_size=AUTOTUNE)
 
+#Creating the model
+
+num_classes = 2
+
+model = Sequential([
+    layers.experimental.preprocessing.Rescaling(1./255,input_shape=(img_height,img_width,3)),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(pool_size=(2,2), strides=None,padding='valid'),
+    layers.Conv2D(32, 3, padding='same',activation='relu'),
+    layers.MaxPooling2D(pool_size=(2,2), strides=None,padding='valid'),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(pool_size=(2,2), strides=None,padding='valid'),
+    layers.Flatten(),
+    layers.Dense(128,activation='relu'),
+    layers.Dense(num_classes)
+    ])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['acuraccy'])
+
+model.summary()
+
+epoch = 10
+
+history = model.fit(train_data,validation_data=test_data,epochs=epoch)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epoch)
+
+plt.figure(figsize=(8,8))
+plt.subplot(epochs_range, acc,label='Training Acuracia')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+
 
